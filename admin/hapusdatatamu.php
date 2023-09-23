5e3e3b8aa9ff0235b5f3e266291b0d88eb0112a1
MochArisRofii <mochrofii827@gmail.com>
2023-09-23 21:24:53 +0700

    BukuTamu

---
+++ admin/hapusdatatamu.php
@@ -0,0 +1,5 @@
+<?php
+    $sql=mysqli_query($config,"DELETE from tamu WHERE noktp='$_GET[id]'");
+
+    echo "<script>alert('Data Berhasil Dihapus');</script>";
+    echo "<script>location='index.php?halaman=datatamu';</script>";
